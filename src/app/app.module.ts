1519f510aa4178863dc4204042aba070ddb69967
PYExpert <Python0522@outlook.com>
2019-07-30 08:32:58 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { DemoMaterialModule } from './material.module'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DemoMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
